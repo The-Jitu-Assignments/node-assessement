26d8a6e8f9700ed2b7f5d5b7722f5024b39b4b99
john katua <johnkatua99@gmail.com>
2022-11-27 12:39:00 +0300

    added a controller to allow user to login

--- E-commerce/Backend/controllers/users/users.js
+++ E-commerce/Backend/controllers/users/users.js
@@ -32,4 +32,39 @@ exports.signUp = async (req, res) => {
       msg: error
     })
   }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const pool = await sql.connect(sqlConfig);
+    const { userEmail, userPassword } = req.body;
+
+    if (!email ||!password) {
+      return res.status(400).json({ error: 'Please fill in all the fields' });
+    };
+
+    const user = await pool.request()
+     .input('userEmail', userEmail)
+     .execute('findUser');
+    
+    if (user) {
+      const comparePassword = await bcrypt.compare(userPassword, user.userPassword);
+      if (comparePassword) {
+        const { userPassword, id, ...payload } = user;
+        const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '24h' });
+        return res.status(200).json({
+          msg: 'User Logged in successfully',
+          token
+        });
+      } else {
+        return res.status(400).json({ msg: 'Password do not match' });
+      }
+    } else {
+      return res.status(404).json({ msg: 'User with that email do not match' });
+    }
+  } catch (e) {
+    return res.status(500).json({
+      msg: e
+    });
+  }
 }
