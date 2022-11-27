edcc5fb8bb849edef98198c3e26c1def17dfa1eb
john katua <johnkatua99@gmail.com>
2022-11-27 11:47:24 +0300

    fetching all users

--- E-commerce/TestQueries/test.sql
+++ E-commerce/TestQueries/test.sql
@@ -26,17 +26,19 @@ GO
 --   receivedEmail INT DEFAULT 0
 -- );
 
-CREATE OR ALTER PROCEDURE usp_signUp(
-  @id VARCHAR(50),
-  @userName VARCHAR(50),
-  @userEmail VARCHAR(50), 
-  @userPassword VARCHAR(50)
-)
-AS
-BEGIN
-  INSERT INTO users (id, userName, userEmail, userPassword)
-  VALUES (@id, @userName, @userEmail, @userPassword);
-END;
+-- CREATE OR ALTER PROCEDURE usp_signUp(
+--   @id VARCHAR(50),
+--   @userName VARCHAR(50),
+--   @userEmail VARCHAR(50), 
+--   @userPassword VARCHAR(50)
+-- )
+-- AS
+-- BEGIN
+--   INSERT INTO users (id, userName, userEmail, userPassword)
+--   VALUES (@id, @userName, @userEmail, @userPassword);
+-- END;
+
+-- select * from users;
 
 -- drop TABLE products
 
