69779d4e0d2789acba18d24514cf3117f4facd5a
john katua <johnkatua99@gmail.com>
2022-11-27 11:41:25 +0300

    users can sign up successfully

--- E-commerce/Backend/Database/store-procedures/users/signUp.sql
+++ E-commerce/Backend/Database/store-procedures/users/signUp.sql
@@ -1,10 +1,11 @@
 CREATE OR ALTER PROCEDURE usp_signUp(
+  @id VARCHAR(50),
   @userName VARCHAR(50),
   @userEmail VARCHAR(50), 
   @userPassword VARCHAR(50)
 )
 AS
 BEGIN
-  INSERT INTO users (userName, userEmail, userPassword)
-  VALUES (@userName, @userEmail, @userPassword);
+  INSERT INTO users (id, userName, userEmail, userPassword)
+  VALUES (@id, @userName, @userEmail, @userPassword);
 END;
