b77b21a666377542a1bcbd03eb92c52e888c3ad9
john katua <johnkatua99@gmail.com>
2022-11-27 12:40:10 +0300

    added a route to login

--- E-commerce/Backend/routes/userRoute.js
+++ E-commerce/Backend/routes/userRoute.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/users/users');
 const router = Router();
 
 router.post('/signup', userController.signUp);
+router.post('/login', userController.login);
 
 module.exports = router;
