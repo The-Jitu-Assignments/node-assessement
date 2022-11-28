31ba995413536f7314aa99cdc3c9ec773b9842d7
john katua <johnkatua99@gmail.com>
2022-11-28 10:58:02 +0300

    initialized cors

--- E-commerce/Backend/server.js
+++ E-commerce/Backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 const  productRoutes  = require('./routes/productsRoute');
 const cartRoutes = require('./routes/cartRoute');
 const userRoutes = require('./routes/userRoute');
@@ -9,6 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/products', productRoutes);
 app.use('/cart', cartRoutes);
