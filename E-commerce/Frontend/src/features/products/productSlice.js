097c292916f465f48b8797eb6949a2d003d8c8d3
john katua <johnkatua99@gmail.com>
2022-11-28 12:27:43 +0300

    cleanedd up the product slice

--- E-commerce/Frontend/src/features/products/productSlice.js
+++ E-commerce/Frontend/src/features/products/productSlice.js
@@ -35,45 +35,15 @@ export const createProduct = createAsyncThunk('product/createProduct',
 export const productSlice = createSlice({
   name: 'product',
   initialState,
-  reducers: {
-    addToCart: (state, action) => {
-      let selectedItem = state.products.find(item => item.id === action.payload);
-      state.cart.push(selectedItem);
-    },
-    increaseItemQuantity: (state, action) => {
-      const updatedCart = state.cart.map((product) => {
-        if (product.id === action.payload) {
-          product.count = product.count + 1
-        }
-        return product
-      })
-      state.cart = updatedCart;
-    },
-    decreaseItemQuantity: (state, action) => {
-      const updatedCart = state.cart.map((product) => {
-        if (product.id === action.payload) {
-          product.count = product.count - 1;
-        }
-        return product
-      })
-      state.cart = updatedCart
-    },
-    removeItemFromCart: (state, action) => {
-      let newArray = state.cart.filter((item) => item.id !== action.payload);
-      state.cart = newArray
-    }
-  },
+  reducers: {},
   extraReducers(builder) {
     builder.addCase(createProduct.fulfilled, (state, action) => {
       state.message = action.payload;
-      // state.products.push(action.payload.data);
     });
     builder.addCase(fetchProducts.fulfilled, (state, action) => {
       state.products = action.payload.data;
     })
   }
 })
-
-export const { addToCart, increaseItemQuantity, decreaseItemQuantity, removeItemFromCart } = productSlice.actions;
 
 export default productSlice.reducer;
